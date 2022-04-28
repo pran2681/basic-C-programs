373474df188525b16444eb1b19bd5f9f94ccf077
Pran Prachurjya Saikia <92747138+pran2681@users.noreply.github.com>
2022-04-28 23:44:00 +0530

    Update find_max.c

--- Basic Quations/find_max.c
+++ Basic Quations/find_max.c
@@ -26,3 +26,33 @@ int main()
     printf("%d",ans);
     return 0;
 }
+
+
+
+
+
+////////////////////////////////
+///alternative
+
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max(int a, int b) {
+    // this is a ternary operator 
+    // conditional ? return value if true : return value if false
+    return a > b ? a : b;    
+}
+
+int max_of_four(int a, int b, int c, int d) {
+    return max(a, max(b, max(c, d)));
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
